da80a3ed324aa7f854b4e8e006e8f92a0a986d9e
naveensharma <sharmanaveen0010@gmail.com>
2023-05-30 15:17:35 +0530

    first commit of nk-blogs

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+
+function App() {
+  return (
+    <div>
+      <nav className="bg-dark p-3">
+      <h1 className="text-start m-2 text-light">Nk Blogs</h1>
+      <ul className="nav-list">
+        <li className="mx-4">Home</li>
+        <li className="mx-4">About</li>
+        <li className="mx-4">Contact us</li>
+      </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default App;
